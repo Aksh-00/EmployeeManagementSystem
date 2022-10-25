7740e52fedd7d892ae0023f485c9f40c20e9d6c1
Aksh-00 <ak.arttoday@gmail.com>
2022-10-25 10:44:11 +0530

    Changes in UI

--- react-frontend/src/Components/HeaderComponent.js
+++ react-frontend/src/Components/HeaderComponent.js
@@ -1,11 +1,10 @@
-
 import React from 'react'
 
 const HeaderComponent = () => {
     return (
         <div>
             <header>
-                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                <nav className = "navbar navbar-expand-md navbar-dark bg-primary">
                     <div>
                         <a href = "https://javaguides.net" className = "navbar-brand">
                             Employee Management Application
